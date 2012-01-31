e850fae65acdb4c171b49d7738ab9dca10beec22
Martin Preisler <mpreisle@redhat.com>
2012-01-31 14:02:36 +0100

    Fixed disk usage check (subshells and variables related)

--- 01_disk_usage.sh
+++ 01_disk_usage.sh
@@ -13,27 +13,22 @@
 [ "$PERCENT_LIMIT_ERROR"   == "" ] && PERCENT_LIMIT_ERROR=100
 
 RET=$XCCDF_RESULT_PASS
-
-df -P $DF_ARGS | 	# list of mounted file systems
-tail -n +2 |	# without first (columns description) line 
-sort -n -k 5 -r |
-
-{
-    while read  device blocks used available_capacity percent_used directory ; do
-	echo "Going to test device $device mounted to $directory"
-	percent_sign='%'
-	percent_used_num_only=${percent_used%$percent_sign}
-	if [ "$percent_used_num_only" -ge "$PERCENT_LIMIT_ERROR" ] ; then
-	  echo "Device $device mounted to $directory is full! There is \$percent_used used!"
-	  echo "Please backup and delete unused files from this directory"
-	  RET=$XCCDF_RESULT_FAIL
-	elif [ "$percent_used_num_only" -ge "$PERCENT_LIMIT_WARNING" ] ; then
-	  echo "Device $device mounted to $directory is going to be full! There is \$percent_used used!"
-	  echo "Please backup and delete unused files from this directory"
-	  [ "$RET" == $XCCDF_RESULT_FAIL ] && RET=$XCCDF_RESULT_INFORMATIONAL
-	fi
-    done
-}
+while read  device blocks used available_capacity percent_used directory ; do
+    percent_sign='%'
+    percent_used_num_only=${percent_used%$percent_sign}
+    
+    if [ "$percent_used_num_only" -ge "$PERCENT_LIMIT_ERROR" ] ; then
+	echo "Device $device mounted to $directory is full! There is $percent_used used!"
+	echo "Please backup and delete unused files from this directory"
+        RET=$XCCDF_RESULT_FAIL
+    elif [ "$percent_used_num_only" -ge "$PERCENT_LIMIT_WARNING" ] ; then
+        echo "Device $device mounted to $directory is going to be full! There is $percent_used used!"
+        echo "Please backup and delete unused files from this directory"
+        [ "$RET" == $XCCDF_RESULT_FAIL ] || RET=$XCCDF_RESULT_INFORMATIONAL
+    fi
+done <<EOF
+`df -P | tail -n +2 | sort -n -k 5 -r`
+EOF
 
 exit $RET
 
