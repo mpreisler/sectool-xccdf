edf6e7d4b1f4e370ec7479aae8a05dadc10d3dd5
Martin Preisler <mpreisle@redhat.com>
2012-02-02 14:43:43 +0100

    Not being root should result in error, not fail. Fixed empty line check. (group check)

--- 01_group.sh
+++ 01_group.sh
@@ -18,7 +18,7 @@ group_shadow=/etc/gshadow
 if [[ $UID -ne '0' ]]
 then
     echo "You have to be logged as root to run this test!"
-    exit ${XCCDF_RESULT_FAIL}
+    exit ${XCCDF_RESULT_ERROR}
 fi
 
 RET=$XCCDF_RESULT_PASS
@@ -76,7 +76,7 @@ do
 	then
 		echo "$shadow: Line $i is empty"
 		echo "Please delete this line."
-		[[ "$RET" == $XCCDF_RESULT_FAIL ]] | RET=$XCCDF_RESULT_INFORMATIONAL
+		[ "$RET" == $XCCDF_RESULT_FAIL ] || RET=$XCCDF_RESULT_INFORMATIONAL
 		continue
 	fi
 
